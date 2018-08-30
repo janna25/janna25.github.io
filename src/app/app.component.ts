620d445266eb82d33e78a97bf840f328e54880c0
Oscar.Pulido <oscar.pulido@MacBook-Pro-de-Admin-5.local>
2018-08-29 22:49:30 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,106 @@
+import { QUESTIONS } from './questions';
 import { Component } from '@angular/core';
 
+declare var $: any;
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'QuienQuiereSerMillonario';
+	question: any;
+	clicked: boolean;
+
+
+	private QUESTIONS: any[];
+	private readonly wrongAnswerClass: string = 'wrong-answer';
+	private readonly rightAnswerClass: string = 'correct-answer';
+
+	points: number = 0;
+
+	constructor() {
+		this.QUESTIONS = Object.assign([], QUESTIONS);
+	}
+
+	ngOnInit(): void {
+		this.question = this.nextQuestion();
+	}
+
+	selectResponse(answer: any, block: HTMLElement): void {
+		this.question.shown = true;
+		this.clicked = true;
+		const correctAnswer: boolean = answer.option == this.question.rightResponse;
+
+		answer.right = correctAnswer;
+
+		if (correctAnswer) {
+			this.onSelectRightResponse(block);
+		} else {
+			this.onSelectWrongResponse(block);
+		}
+
+
+	}
+
+
+	private onSelectRightResponse(block: HTMLElement): void {
+		setTimeout(() => {
+			$(block).toggleClass(this.rightAnswerClass);
+			setTimeout(() => {
+				$(block).toggleClass(this.rightAnswerClass);
+				setTimeout(() => {
+					$(block).toggleClass(this.rightAnswerClass);
+					setTimeout(() => {
+						$(block).toggleClass(this.rightAnswerClass);
+						setTimeout(() => {
+							$(block).toggleClass(this.rightAnswerClass);
+							this.points += 10;
+
+							if (this.points < 60) {
+								$('.correct-modal').modal('show');
+							} else {
+								$('.complete-modal').modal('show');
+							}
+						}, 100);
+					}, 100);
+				}, 100);
+			}, 100);
+		}, 100);
+	}
+
+	private onSelectWrongResponse(block: HTMLElement): void {
+		setTimeout(() => {
+			$(block).toggleClass(this.wrongAnswerClass);
+			setTimeout(() => {
+				$(block).toggleClass(this.wrongAnswerClass);
+				setTimeout(() => {
+					$(block).toggleClass(this.wrongAnswerClass);
+					setTimeout(() => {
+						$(block).toggleClass(this.wrongAnswerClass);
+						setTimeout(() => {
+							$(block).toggleClass(this.wrongAnswerClass);
+							$('.wrong-modal').modal('show');
+						}, 100);
+					}, 100);
+				}, 100);
+			}, 100);
+		}, 100);
+
+	}
+
+
+	onAccept() {
+		this.question = this.nextQuestion() || {};
+	}
+
+
+	private nextQuestion(): any {
+		return this.QUESTIONS.find((q: any) => !q.shown);
+	}
+
+
+	resetAll(): void {
+		location.reload();
+	}
 }
---
+++ src/app/questions.ts
@@ -0,0 +1,67 @@
+export const QUESTIONS: any[] = [
+    {
+        question: 'LAS NUEVAS CIENCIAS QUE NOS TRAEN',
+        answers: [
+            { option: 'a', response: 'El estudio de una parcela grande.' },
+            { option: 'b', response: 'El estudio de todo organizador de los saberes' },
+            { option: 'c', response: 'El estudio de un latifundio de alta comlejidad para una cabeza bien puesta' },
+            { option: 'd', response: 'El estudio de un sistema complejo' }
+        ],
+        rightResponse: 'd',
+        shown: false
+    },
+    {
+        question: 'CUAL DE LAS SIGUIENTES AFIRMACIONES NO ES LA CORRECTA EN LA ARGUMENTACION DE LA PALABRA ARS COGITANDY',
+        answers: [
+            { option: 'a', response: 'Lugar para el buen arte del pensar profundo, ajen, propio siempre con el sello del buen pensar.' },
+            { option: 'b', response: 'La lógica no forma, pues, la razón del hombre sin la razón del sabio.' },
+            { option: 'c', response: 'El buen uso de la lógica de la deducción, de la inducción el arte de la argumentación y de la discusión.' },
+        ],
+        rightResponse: 'b',
+        shown: false
+    },
+    {
+        question: 'CUÁL ES EL PRIMER VINCULO QUE ESTABLECE UN NUDO ENTRE LO BIOLÓGICO Y LO HUMANO',
+        answers: [
+            { option: 'a', response: 'Entre la computadora y ciencias cognitivas.' },
+            { option: 'b', response: 'Entre el cerebro y la computadora.' },
+            { option: 'c', response: 'Entre ciencias cognitivas y ciencias del hombre.' },
+            { option: 'd', response: 'Entre ciencias de la vida y ciencias humanas.' }
+        ],
+        rightResponse: 'd',
+        shown: false
+    },
+    {
+        question: 'QUÉ ES EL CONOCIMIENTO, SEGÚN SU ORGANIZACIÓN',
+        answers: [
+            { option: 'a', response: 'Traducción – Reconstrucción, Separación – Unión, Análisis - Síntesis.' },
+            { option: 'b', response: 'Saber mucho, construyendo.' },
+            { option: 'c', response: 'Tradición – mejoramiento, Lectura – Escritura, Sumar – Restar.' }
+        ],
+        rightResponse: 'a',
+        shown: false
+    },
+    {
+        question: 'QUIÉN FUE EL CREADOR DEL IMPERATIVO DE PASCAL Y EN QUÉ AÑO LO PUBLICÓ',
+        answers: [
+            { option: 'a', response: 'Alan Turing, 198.' },
+            { option: 'b', response: 'Dennis Ritchie, 1920.' },
+            { option: 'c', response: 'Fernando vallejo, 1995.' },
+            { option: 'd', response: 'Niklas Wirth, 1970.' }
+        ],
+        rightResponse: 'd',
+        shown: false
+    },
+    {
+        question: 'CUÁL DE LOS SIGUIENTES FILÓSOFOS NOS DICE',
+        extraInfo: 'nos encaminamos hacia una concepción geo-bio-física de la tierra en la que se integran sistémicamente las características físicas de origen biológico (oxigeno del aire, el calcáreo etc.) en el que la vida no es solo un producto si no un actor de la física terrestres',
+        answers: [
+            { option: 'a', response: 'Westbroek.' },
+            { option: 'b', response: 'Ratzell.' },
+            { option: 'c', response: 'Jean-Pierre Allix.' },
+            { option: 'd', response: 'Ninguna de las anteriores.' }
+        ],
+        rightResponse: 'a',
+        shown: false
+    },
+];
